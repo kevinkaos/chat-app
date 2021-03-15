adef98e6ccf557292c254c60b6290b4e92a355fd
Kevin Kao <namedkk126@gmail.com>
2021-03-15 14:26:29 +0800

    add channel validation

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
 import { Formik, Form as FormikForm, Field } from "formik";
+import * as Yup from "yup";
 
 const Channels = () => {
   const [channels, setChannels] = useState([]);
   const [modal, setModal] = useState(false);
+  const channelSchema = Yup.object().shape({
+    channelName: Yup.string().required("Please provide a channel name."),
+    channelDetails: Yup.string().required(
+      "Please provide some details about the channel."
+    ),
+  });
   return (
     <>
       <Menu.Menu style={{ padding: "1rem 0" }}>
@@ -21,45 +28,67 @@ const Channels = () => {
         </Menu.Item>
       </Menu.Menu>
 
-      <Modal basic size="small" open={modal} onClose={() => setModal(false)}>
-        <Modal.Header>Add a Channel</Modal.Header>
-        <Modal.Content>
-          <Formik
-            initialValues={{
-              channelName: "",
-              channelDetails: "",
+      <Formik
+        initialValues={{
+          channelName: "",
+          channelDetails: "",
+        }}
+        validationSchema={channelSchema}
+      >
+        {({ isSubmitting, isValid, dirty, resetForm }) => (
+          <Modal
+            basic
+            size="small"
+            open={modal}
+            onClose={() => {
+              setModal(false);
+              resetForm();
             }}
           >
-            <Form as={FormikForm}>
-              <Form.Field>
-                <Field
-                  as={Input}
-                  fluid
-                  label="Name of Channel"
-                  name="channelName"
-                />
-              </Form.Field>
-              <Form.Field>
-                <Field
-                  as={Input}
-                  fluid
-                  label="About the Channel"
-                  name="channelDetails"
-                />
-              </Form.Field>
-            </Form>
-          </Formik>
-        </Modal.Content>
-        <Modal.Actions>
-          <Button color="green" inverted>
-            <Icon name="checkmark" />
-            Add
-          </Button>
-          <Button color="red" inverted onClick={() => setModal(false)}>
-            <Icon name="remove" /> Cancel
-          </Button>
-        </Modal.Actions>
-      </Modal>
+            <Modal.Header>Add a Channel</Modal.Header>
+            <Modal.Content>
+              <Form as={FormikForm}>
+                <Form.Field>
+                  <Field
+                    as={Input}
+                    fluid
+                    label="Name of Channel"
+                    name="channelName"
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <Field
+                    as={Input}
+                    fluid
+                    label="About the Channel"
+                    name="channelDetails"
+                  />
+                </Form.Field>
+              </Form>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button
+                disabled={isSubmitting || !(isValid && dirty)}
+                color="green"
+                inverted
+              >
+                <Icon name="checkmark" />
+                Add
+              </Button>
+              <Button
+                color="red"
+                inverted
+                onClick={() => {
+                  setModal(false);
+                  resetForm();
+                }}
+              >
+                <Icon name="remove" /> Cancel
+              </Button>
+            </Modal.Actions>
+          </Modal>
+        )}
+      </Formik>
     </>
   );
 };
