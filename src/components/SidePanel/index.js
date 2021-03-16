3b4387eb9380bab99ee8efc37cb68410588b9a49
Kevin Kao <namedkk126@gmail.com>
2021-03-16 15:33:16 +0800

    remove key for channel component

--- src/components/SidePanel/index.js
+++ src/components/SidePanel/index.js
@@ -14,7 +14,7 @@ const SidePanel = ({ currentUser }) => {
       style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
     >
       <UserPanel currentUser={currentUser} />
-      <Channels key={currentUser.uid} currentUser={currentUser} />
+      <Channels currentUser={currentUser} />
       <DirectMessages key={currentUser.uid} currentUser={currentUser} />
     </Menu>
   );
